77c2a7f4c9ac6a1c29a638125ea51bda3722b877
Krish Patel <53786660+kxp9343@users.noreply.github.com>
2019-08-09 15:58:28 -0500

    Add files via upload

---
+++ millionare.py
@@ -0,0 +1,16 @@
+print("****************************************")
+print("Welcome to Who Wants to Be a Millionare!")
+print("****************************************")
+
+print("In the children's book series, where is Paddington Bear originally from?")
+print("A: India")
+print("B: Peru") 
+print("C: Canada") 
+print("D: Iceland")
+
+print("What letter must apperar at the beginning of the registration of all non-militart aircraft in the U.S?")
+print("A: N") 
+print("B:  A") 
+print("C: U")  
+print("D: L")
+
